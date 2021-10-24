d51518cb14ce1917e0ec8fd9ab3a415fc5463663
kongura <kongura114@gmail.com>
2021-10-24 17:33:02 +0900

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
-import ToDoListItem from "./ToDoListItem.js"
 
 class App extends Component {
   constructor(props) {
